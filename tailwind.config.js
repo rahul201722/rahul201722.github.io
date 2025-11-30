a12e38e19c696374cf80b8aa3e8636aef99d4d58
Rahul Ranjan <67601359+rahul201722@users.noreply.github.com>
2025-11-30 22:21:08 +0530

    Updated some words

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#54B689',
+        'primary-dark': '#3d9870',
+      }
+    },
+  },
+  plugins: [],
+}
